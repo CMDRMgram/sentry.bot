a2b30130058126a64839db40f2ae7d3fbf57b84f
Medi0cr3 <testfax.medi0cr3@gmail.com>
2024-11-03 16:28:23 -0700

    update

--- commands/common/timegen.js
+++ commands/common/timegen.js
@@ -3,12 +3,10 @@ const { eventTimeValidate } = require('../../functions')
 
 const config = require('../../config.json')
 
-let date = new Date();
+let date = new Date(Date.now() + 45000)
 let diff = Math.round((new Date() - date) / 1000)
 var rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
 
-// const timestamp = eventTimeValidate("30/Apr 10:30",-6);
-// console.log(timestamp);
 
 const timeGen = {
     default: { month: 'short', day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false },
@@ -20,7 +18,6 @@ const timeGen = {
     longDateTime: { weekday: 'short', month: 'numeric', day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', hour12: false },
     relativeTime: rtf.format(-diff, 'second')
 }
-
 module.exports = {
     data: new Discord.SlashCommandBuilder()
     .setName(`timegen`)
@@ -37,22 +34,22 @@ module.exports = {
                 {name: `Long Date: ${date.toLocaleString('en-US', { timeZone: 'UTC', ...timeGen.longDate })}`, value: 'D'},
                 {name: `Short Date/Time: ${date.toLocaleString('en-US', { timeZone: 'UTC', ...timeGen.shotDateTime })}`, value: 'f'},
                 {name: `Long Date/Time: ${date.toLocaleString('en-US', { timeZone: 'UTC', ...timeGen.longDateTime })}`, value: 'F'},
-                {name: `Relative Time: ${date.toLocaleString('en-US', { timeZone: 'UTC', ...timeGen.relativeTime })}`, value: 'R'}
+                {name: `Relative Time: ${timeGen.relativeTime}`, value: 'R'}
             )
         )
     .addNumberOption(option => 
         option.setName('timezone')
-            .setDescription('Enter your current timezone: Example: 0,-5,+2')
+            .setDescription('Timezone offset: Example: 0,-5,+2. Check google if you dont know.')
             .setRequired(true)
     )
     .addStringOption(option => 
         option.setName('datetime')
-            .setDescription('Enter the date-time in your local time in this format only: 15/Jan 15:30')
+            .setDescription('Enter the date-time in your local time in this format only: 15/Jan 1530')
             .setRequired(true)
     )
     ,
     async execute(interaction) {
-        await interaction.deferReply({ ephemeral: true })
+        await interaction.deferReply({ ephemeral: false })
         let inputs = interaction.options._hoistedOptions
         let timeFormat = inputs.find(i => i.name === 'type').value
         let timeValue = inputs.find(i => i.name === 'datetime').value
@@ -62,7 +59,8 @@ module.exports = {
         const time_unformatted = timeFormat == 'a' ? '```<t:' + timestamp + '>```' : '```<t:' + timestamp + ':' + timeFormat + '>```';
         const embed = new  Discord.EmbedBuilder()
             .setTitle('Custom Time')
-            .setDescription('Created a discord timestamp from your chosen local time inputed.')
+            .setColor('#00FFFF') //bright cyan
+            .setDescription('Created a discord timestamp from your chosen local time.')
             .addFields(
                 {name: 'Your local time input:', value: timeValue, inline: true},
                 {name: 'Your local time visual', value: time, inline: true},
