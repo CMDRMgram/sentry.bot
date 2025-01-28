6528826a994e3232387705601d929fb8ecf9c64a
vorsipellis <versipellis@hey.com>
2025-01-28 00:12:31 -0500

    Update preengineered.js

--- commands/Warden/ax-info/preengineered.js
+++ commands/Warden/ax-info/preengineered.js
@@ -8,16 +8,17 @@ module.exports = {
     permissions:0,
     execute (interaction) {
         const returnEmbed = new Discord.EmbedBuilder()
-        .setTitle('Pre-engineered Modules')
+        .setTitle('Pre-Engineered Modules')
         .setURL('https://wiki.antixenoinitiative.com/en/weapons')
         .setAuthor({name: botIdent().activeBot.botName,iconURL: botIdent().activeBot.icon})
-        .setDescription(`It should be noted that **all pre-engineered modules require materials for each purchase**.
-          **Modified Guardian Weapons** are available at **Prospect's Deep**, a planetary port found in the **Mbooni** system.
-          You will need a permit to access Mbooni, which can be earned at the "Glorious Prospect" in LHS 1163 through work for Azimuth Biotech.
-          The **V1 Frame Shift Drive** can be obtained at any human tech broker.
-          **Pre-engineered Heatsinks** can be found at Tech Brokers on the Sirius megaships listed below.
-          Unlock costs for these can be found on Inara's crafting section.`)
-        .setImage('https://cdn.discordapp.com/attachments/832092794293190706/1060318125943439380/image.png')
+        .setDescription(`It should be noted that **all pre-engineered modules require materials for each purchase**. Unlock costs for these can be found on Inara's crafting section.`)
+        .addFields(
+            {name: "Modified Guardian Weapons, Azimuth EAXMC", value: "Available at **Prospect's Deep**, a planetary port found in the **Mbooni** system. You will need a permit to access Mbooni, which can be earned through raising standings to Allied with Azimuth Biotech. You may need to visit the `Glorious Prospect` in `LHS 1163`."},
+            {name: "Sirius AX Missile Rack, Sirius Heat Sinks", value: "Can be found at Sirius Tech Brokers (see image below)."},
+            {name: "Azimuth EAXMC", value: "Can only be found at Rescue Ships (`Rescue Ship Hutner` in `Luyten's Star` and `Rescue Ship Cornwallis` in `V886 Centauri`)."},
+            {name: "Frame Shift Drive (SCO)", value: "Can be found at Human Tech Brokers (see image below)."},   
+        )
+        .setImage('https://wiki.antixenoinitiative.com/img/techbrokers.png')
         interaction.reply({embeds: [returnEmbed.setTimestamp()]})
     }
 }
