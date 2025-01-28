4733973d40cd18af745e6f9fc514e0d44dd4b1a1
vorsipellis <versipellis@hey.com>
2025-01-28 00:18:09 -0500

    Update preengineered.js

--- commands/Warden/ax-info/preengineered.js
+++ commands/Warden/ax-info/preengineered.js
@@ -13,10 +13,10 @@ module.exports = {
         .setAuthor({name: botIdent().activeBot.botName,iconURL: botIdent().activeBot.icon})
         .setDescription(`It should be noted that **all pre-engineered modules require materials for each purchase**. Unlock costs for these can be found on Inara's crafting section.`)
         .addFields(
-            {name: "Modified Guardian Weapons, Azimuth EAXMC", value: "Available at `Prospect's Deep`, a planetary port found in the `Mbooni` system. You will need a permit to access Mbooni, which can be earned through raising standings to Allied with Azimuth Biotech. You may need to visit the `Glorious Prospect` in `LHS 1163`."},
-            {name: "Sirius AX Missile Rack, Sirius Heat Sinks", value: "Can be found at Sirius Tech Brokers (see image below)."},
-            {name: "Azimuth EAXMC", value: "Can only be found at Rescue Ships (`Rescue Ship Hutner` in `Luyten's Star` and `Rescue Ship Cornwallis` in `V886 Centauri`)."},
-            {name: "Frame Shift Drive (SCO)", value: "Can be found at Human Tech Brokers (see image below)."},   
+            {name: "Modified Guardian Weapons, Azimuth EAXMC", value: "Available at `Prospect's Deep`, a planetary port found in the `Mbooni` system. You will need a permit to access Mbooni, which can be earned through raising standings to Allied with Azimuth Biotech. You may need to visit the `Glorious Prospect` in `LHS 1163`.", inline: false},
+            {name: "Sirius AX Missile Rack, Sirius Heat Sinks", value: "Can be found at Sirius Tech Brokers (see image below).", inline: false},
+            {name: "Azimuth EAXMC", value: "Can only be found at Rescue Ships (`Rescue Ship Hutner` in `Luyten's Star` and `Rescue Ship Cornwallis` in `V886 Centauri`).", inline: false},
+            {name: "Frame Shift Drive (SCO)", value: "Can be found at Human Tech Brokers (see image below).", inline: false},   
         )
         .setImage('https://wiki.antixenoinitiative.com/img/techbrokers.png')
         interaction.reply({embeds: [returnEmbed.setTimestamp()]})
