b2eda0d920ac185d43249b0ffadf9e68406ed67d
vorsipellis <versipellis@hey.com>
2025-01-27 23:28:45 -0500

    Update soloceptor.js

--- commands/Warden/wiki/soloceptor.js
+++ commands/Warden/wiki/soloceptor.js
@@ -13,7 +13,7 @@ module.exports = {
 		.setTitle("**Solo Interceptor Hunting**")
 		.setDescription(`The following are systems that we recommend for solo interceptor duels:`)
         .addFields({name: "`Asterope`, `Merope`, and `Sterope II`", value: "Our go-to hunting grounds in the Pleiades Nebula. Has stations if you don't have a Fleet Carrier and are close to the Bubble. Sterope II is a recommended Hydra hunting ground."},
-		   {name: "`Musca Dark Region HM-V c2-17`", value: "No station in-system or nearby, but contains high spawn density of higher-threat NHSSes if you have a Fleet Carrier to operate out of. Also has material traders within approximately ~30 LY's."},
+		   {name: "`Musca Dark Region HM-V c2-17`", value: "Coalsack Nebula. No station in-system or nearby, but contains high spawn density of higher-threat NHSSes if you have a Fleet Carrier to operate out of. Also has material traders within approximately ~30 LY's."},
 		  {name: "`Pleiades Sector MI-S B4-0`", value: "Historically known as The Zoo, it used to be the only system where solo Hydras could be found; it is still a recommended Hydra hunting ground."})
 
         const buttonRow = new Discord.ActionRowBuilder()
